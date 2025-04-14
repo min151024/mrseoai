723b72d2024354ea1c26bbfd7f8fd63ae517ab80
min151024 <h22716nm@gm.tsuda.ac.jp>
2025-04-14 16:32:10 +0900

    delate ge-

--- prepare_credentials.py
+++ prepare_credentials.py
@@ -5,8 +5,3 @@ import base64
 if "GOOGLE_CREDS_BASE64" in os.environ:
     with open("credentials.json", "wb") as f:
         f.write(base64.b64decode(os.environ["GOOGLE_CREDS_BASE64"]))
-
-# ga_credentials.json を作成
-if "GA_CREDS_BASE64" in os.environ:
-    with open("ga_credentials.json", "wb") as f:
-        f.write(base64.b64decode(os.environ["GA_CREDS_BASE64"]))
