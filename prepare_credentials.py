557fa7a3083e64d161e9d3678eb9237a2c92911b
min151024 <h22716nm@gm.tsuda.ac.jp>
2025-05-19 16:24:51 +0900

    ignore 変更

--- prepare_credentials.py
+++ prepare_credentials.py
@@ -1,7 +1,7 @@
 import os
 import base64
 
-# credentials.json を作成
+
 if "GOOGLE_CREDS_BASE64" in os.environ:
     with open("credentials.json", "wb") as f:
         f.write(base64.b64decode(os.environ["GOOGLE_CREDS_BASE64"]))
