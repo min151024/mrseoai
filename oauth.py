494261a7f1a976f14d1dfa60a3a821dbd0e88ac0
min151024 <h22716nm@gm.tsuda.ac.jp>
2025-05-24 15:33:02 +0900

    base64設定

--- oauth.py
+++ oauth.py
@@ -1,15 +1,18 @@
-# oauth.py
+import base64
+from pathlib import Path
 import os
-import json
-import pathlib
-import requests
 from flask import session, redirect, request, url_for
 from google_auth_oauthlib.flow import Flow
 from google.oauth2.credentials import Credentials
 
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # 開発用。httpsでないときでも許可。
-
-CLIENT_SECRETS_FILE = "client_secret.json"
+b64 = os.getenv("GOOGLE_OAUTH2_CLIENT_SECRET_JSON_BASE64")
+if b64:
+    credentials_path = Path(__file__).parent / "client_secret.json"
+    credentials_path.write_bytes(base64.b64decode(b64))
+    CLIENT_SECRETS_FILE = str(credentials_path)
+else:
+    # ローカル等でファイル配置している場合
+    CLIENT_SECRETS_FILE = "client_secret.json"
 SCOPES = [
     "https://www.googleapis.com/auth/webmasters.readonly",  # GSC読み取り
     "https://www.googleapis.com/auth/analytics.readonly"    # GA読み取り
