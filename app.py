f8bc258d0aeed33dec9c18bcbc8be25b6adf76ae
min151024 <h22716nm@gm.tsuda.ac.jp>
2025-05-29 19:20:17 +0900

    関数名編集

--- app.py
+++ app.py
@@ -123,11 +123,11 @@ def result():
 
 
 @app.route("/privacy")
-def privacy_policy():
+def privacy():
     return render_template("privacy.html")
 
 @app.route("/terms")
-def terms_of_service():
+def terms():
     return render_template("terms.html")
 
 if __name__ == "__main__":
